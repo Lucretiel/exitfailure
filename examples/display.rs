11c5f5c52d5598ce9d3c66f8d22a5f5108740fe0
Toby Smith <toby@tismith.id.au>
2018-07-30 12:38:39 +1000

    wip: trial a boxed exitdisplay instead of exitdisplay<T>

--- examples/display.rs
+++ examples/display.rs
@@ -2,10 +2,10 @@ extern crate exitfailure;
 
 use exitfailure::ExitDisplay;
 
-fn main() -> Result<(), ExitDisplay<String>> {
+fn main() -> Result<(), ExitDisplay> {
     Ok(some_fn()?)
 }
 
-fn some_fn() -> Result<(), String> {
-    Err("this is an error message".into())
+fn some_fn() -> Result<(), &'static str> {
+    Err("this is an error message")
 }
